bfa2c8c576f521b7e3464c679e708538496c9302
Pablo <Pablo@PJLV.home>
2024-04-12 16:47:01 +0200

    PARTE B) TERMINADA

--- src/main/Start.java
+++ src/main/Start.java
@@ -1,11 +1,17 @@
 package main;
 
+import java.util.ArrayList;
+
 import ficheros.FicheroReader;
 
 public class Start {
 
 	public static void main(String[] args) {
 		
-		FicheroReader.procesarDatos(".//datos.txt");
+		
+		
+		ArrayList<Integer> c = FicheroReader.procesarDatos(".//datos.txt");
+		System.out.println(c);
+		
 	}
 }
